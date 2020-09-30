55daaf6b6d99739a16ca5518b1d675e7929443ed
Sumit Patel <sumitz1207@gmail.com>
2020-09-30 17:13:30 -0400

    add PyParagraph

---
+++ PyParagraph/main.py
@@ -0,0 +1,33 @@
+import os
+import csv
+#regex to find paragraph's setence lengths
+import re
+
+#create lists to store paragraph info
+para_size = []
+para_words = []
+para_sent = []
+para_val = []
+#store user input to find text file to analyze
+fileP = os.path.join(input("Please type your text file location: "))
+with open(fileP, "r") as file:
+    paragraph = file.read()
+    #find word count by finding spaces
+    para_words = (len(paragraph.split(" ")))
+    #find paragraph char size by finding length of paragraph string
+    para_size = len(paragraph)
+    #find sentence length using regex on paragraph
+    para_sent = len(re.split("(?<=[.!?]) +", paragraph))
+    #sentence length is the word count divided by sentence count
+    sent_length = str(para_words/para_sent)
+    #letter count is the size of the paragraph divided by the word count
+    letter_count = str(para_size/para_words)
+    #print out all paragraph analysis information
+    print("\n")
+    print("Paragraph Analysis")
+    print("_________________________")
+    print("Approx. Word Count: " + str(para_words))
+    print("Approx. Sentence Count: " + str(para_sent))
+    print("Average Letter Count: " + letter_count)
+    print("Average Sentence Length: " + sent_length)
+
--- README.md
+++ README.md
@@ -4,6 +4,6 @@ PyBank calculates the monthly financial analysis of a company.
 
 PyPoll calculates the winner and other election results of a local election between 4 candidates.
 
-PyBoss updates a csv list of employees to a new style format
+PyBoss updates a csv list of employees to a new style format.
 
-I have not completed PyParagraph
+PyParagraph analyzes sentence and word counts and length within a text file.
